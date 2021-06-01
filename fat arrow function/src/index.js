dabcb92793ae694731836125484bea84236dcf19
Areeb Bin Azim <areebbinazim360@gmail.com>
2021-06-01 19:04:11 +0500

    initial files of fat arrow functions

---
+++ fat arrow function/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+const newNumbers = numbers.map(x => x * x);
+
+// __________________________________________________________________________________
+
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map( x => x * 2);
+
+// _________________________________________________________________________________________
+
+////Filter - Create a new array by keeping the items that return true.
+const newNumbers = numbers.filter(num => num < 10);
+
+// _____________________________________________________________________________________________
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+const newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+
+// ________________________________________________________________________________________________
+
+//Find - find the first item that matches from an array.
+const newNumber = numbers.find(num => num > 10);
+
+// ______________________________________________________________________________________________
+
+//FindIndex - find the index of the first item that matches.
+const newNumber = numbers.findIndex(num => num > 10);
+
+
+// ___________________________________________________________________________________________________
